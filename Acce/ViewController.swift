3cd9c5278a706ebd7ff7ff669076f7e3157708b2
zacalex <qinjiahuangus@gmail.com>
2017-11-27 11:53:20 -0800

    basic

--- Acce/ViewController.swift
+++ Acce/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 import CoreMotion
 class ViewController: UIViewController {
-
+    var motionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to : OperationQueue.current!){
+            (data, error) in
+            if let myData = data {
+                print(myData)
+            }
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
